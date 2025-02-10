d9709001c6d4529680b5d6e2f49938d534d38c64
Kanishk Gandhi <kanishk050296@gmail.com>
2025-02-10 02:19:11 -0800

    debug

--- swebench/harness/constants/constants.py
+++ swebench/harness/constants/constants.py
@@ -52,9 +52,9 @@ KEY_MODEL = "model_name_or_path"
 KEY_PREDICTION = "model_patch"
 
 # Constants - Harness
-DOCKER_PATCH = "/tmp/patch.diff"
+DOCKER_PATCH = "/scr/kanishkg/tmp/patch.diff"
 DOCKER_USER = "root"
-DOCKER_WORKDIR = "/testbed"
+DOCKER_WORKDIR = "/scr/kanishkg/testbed"
 LOG_REPORT = "report.json"
 LOG_INSTANCE = "run_instance.log"
 LOG_TEST_OUTPUT = "test_output.txt"
